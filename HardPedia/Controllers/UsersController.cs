7787206978a1a6ba9f9da9eab4c2ff3eab86e9c9
bogdancomardici <115560133+bogdancomardici@users.noreply.github.com>
2024-05-14 15:36:44 +0300

    Add project files.

---
+++ HardPedia/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using HardPedia.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HardPedia.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ILogger<HomeController> logger, ApplicationDbContext appContext)
+        {
+            _logger = logger;
+            _context = appContext;
+        }
+
+        
+
+        public IActionResult ListUsers()
+        {   
+            List<IdentityUser> users = _context.Users.ToList();
+            return View(users);
+        }
+
+
+    }
+}
---
+++ HardPedia/Program.cs
@@ -0,0 +1,56 @@
+using HardPedia.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var roles = new[] {"Registered", "Moderator", "Admin"};
+
+    foreach (var role in roles)
+    {
+        if (!roleManager.RoleExistsAsync(role).Result)
+        {
+            roleManager.CreateAsync(new IdentityRole(role)).Wait();
+        }
+    }
+}
+
+
+app.Run();
