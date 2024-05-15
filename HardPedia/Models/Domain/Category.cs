58007d3f74753631e143178160a08292c26e2ab6
TimiAndrei <timiandrei223@gmail.com>
2024-05-15 22:46:00 +0300

    added Subject and Category models

---
+++ HardPedia/Models/Domain/Category.cs
@@ -0,0 +1,15 @@
+﻿namespace HardPedia.Models.Domain;
+
+public class Category
+{
+
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string DisplayName { get; set; }
+
+    public string? Description { get; set; }
+
+    public ICollection<Subject> Subjects { get; set; }
+}
---
+++ HardPedia/Models/Domain/Subject.cs
@@ -0,0 +1,25 @@
+﻿namespace HardPedia.Models.Domain;
+
+
+public class Subject
+{
+    public Guid Id { get; set; }
+
+    public string Heading { get; set; }
+
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public string UrlHandle { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public string Author { get; set; }
+
+    public bool Visible { get; set; }
+
+    public ICollection<Category> Categories { get; set; }
+
+
+}
