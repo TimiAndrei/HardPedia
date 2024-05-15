cf73619adce0d33edb51bffcc4cfa1b85daeedb6
TimiAndrei <timiandrei223@gmail.com>
2024-05-15 23:40:14 +0300

    updated home page to list categories

--- HardPedia/Controllers/HomeController.cs
+++ HardPedia/Controllers/HomeController.cs
@@ -1,21 +1,42 @@
+using HardPedia.Data;
 using HardPedia.Models;
+using HardPedia.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace HardPedia.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int pageNumber = 1, int pageSize = 10)
         {
-            return View();
+            var categories = await _context.Categories
+                .Include(c => c.Subjects)
+                .ToListAsync();
+
+            var categoryViewModels = categories.Select(c => new CategorySubjectsViewModel(
+                c.Id,
+                c.Name,
+                c.Description,
+                c.Subjects.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToList(),
+                (int)Math.Ceiling(c.Subjects.Count / (double)pageSize),
+                pageNumber
+            )).ToList();
+
+            return View(categoryViewModels);
         }
 
         public IActionResult Privacy()
---
+++ HardPedia/Models/ViewModels/CategorySubjectsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using HardPedia.Models.Domain;
+
+namespace HardPedia.Models.ViewModels;
+
+public class CategorySubjectsViewModel
+{
+    public Guid CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string? CategoryDescription { get; set; }
+    public List<Subject> Subjects { get; set; }
+    public int TotalPages { get; set; }
+    public int CurrentPage { get; set; }
+
+    public CategorySubjectsViewModel(Guid categoryId, string categoryName, string? categoryDescription, List<Subject> subjects, int totalPages, int currentPage)
+    {
+        CategoryId = categoryId;
+        CategoryName = categoryName;
+        CategoryDescription = categoryDescription;
+        Subjects = subjects;
+        TotalPages = totalPages;
+        CurrentPage = currentPage;
+    }
+}
